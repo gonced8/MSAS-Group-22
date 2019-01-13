2a54ab65f6f6201fce848133beb7b03f116c6776
Gonçalo Raposo <gonced_8@hotmail.com>
2019-01-13 16:10:08 +0100

    correction folder optimization results

--- optimization/optimization.m
+++ optimization/optimization.m
@@ -6,7 +6,7 @@ addContainingDirAndSubDir();
 disp('Optimization started');
 start = tic;
 [par, err] = optimize();
-save('results_optimization.mat', 'par', '-v7.3');
+save('optimization/results_optimization.mat', 'par', '-v7.3');
 toc(start);
 disp('Optimization ended');
 
